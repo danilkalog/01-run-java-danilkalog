eb1f208227c273dce44e33c9d2a5d5ec701f4db2
danilkalog <pisma4dan4ik@gmail.com>
2026-02-18 11:42:23 +0300

    Версия обновленная

--- ru/hse/lab1/Hello.java
+++ ru/hse/lab1/Hello.java
@@ -1,7 +1,13 @@
 package ru.hse.lab1;
-
+/**
+ * @author Логинов Данила Александрович
+ * @version 1.1
+ * @since 2026
+ */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+
 }
